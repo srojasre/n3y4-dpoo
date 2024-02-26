63ae754609fb150ea8483bccdad7d2969ec96bdd
srojasre <s.rojasr2@uniandes.edu.co>
2024-02-25 21:10:47 -0500

    update2

--- nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/tiquetes/Tiquete.java
+++ nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/tiquetes/Tiquete.java
@@ -1,5 +1,48 @@
 package uniandes.dpoo.aerolinea.tiquetes;
+import uniandes.dpoo.aerolinea.modelo.Vuelo;
+import uniandes.dpoo.aerolinea.modelo.Cliente;
+
 
 public class Tiquete {
+    
+    private String codigo;
+    private int tarifa;
+    private boolean usado;
+    private Vuelo vuelo;
+    private Cliente clienteComprador;
 
+  
+    public Tiquete(String codigo, Vuelo vuelo, Cliente clienteComprador, int tarifa) {
+        this.codigo = codigo;
+        this.vuelo = vuelo;
+        this.clienteComprador = clienteComprador;
+        this.tarifa = tarifa;
+        this.usado = false; 
+    }
+
+
+    public Cliente getCliente() {
+        return clienteComprador;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getTarifa() {
+        return tarifa;
+    }
+
+    public void marcarComoUsado() {
+        this.usado = true;
+    }
+
+   
+    public boolean esUsado() {
+        return usado;
+    }
 }
